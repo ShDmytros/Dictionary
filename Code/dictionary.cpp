dda2b795ff82cd6fe833499897952f0a99691ee2
ShDmytros <160020732+ShDmytros@users.noreply.github.com>
2025-01-19 15:02:48 +0200

    second commit

--- Code/dictionary.cpp
+++ Code/dictionary.cpp
@@ -7,8 +7,17 @@
 using namespace std;
 
 
+class GraphicSettings
+{
+public:
+	void setColor(int color) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color); }
+};
+
+
 class WorkingWithWords
 {
+	GraphicSettings setting;
+
 public:
 	void FindAWord(map<string, string>& dict)
 	{
@@ -19,7 +28,9 @@ public:
 
 		string learning_language_word;
 
+		setting.setColor(10);
 		cout << "Type a learning word that you want to find: ";
+		setting.setColor(7);
 		cin >> learning_language_word;
 
 		auto seeking_word = dict.find(learning_language_word);
@@ -27,8 +38,11 @@ public:
 			cout << seeking_word->first << " - " << seeking_word->second << endl;
 		else
 		{
-			cout << "Sorry we couldn't found the word, maybe"
-				"it's not exist, please type 2 and write it in this case." << endl;
+			setting.setColor(4);
+			cout << "Sorry we couldn't found the word, maybe" << endl;
+			cout << "it's not exist. Please type 2 and write it," << endl;
+			cout << "if you made a mistake durring typing." << endl;
+			setting.setColor(7);
 		}
 	}
 
@@ -38,20 +52,23 @@ public:
 		string learning_language_word;
 		string native_language_word;
 
-		for (auto it = dict.begin(); it != dict.end(); ++it)
-		{
-			if (it->first == learning_language_word)
-			{
-				cout << "Sorry the word have exist!" << endl;
-				return;
-			}
-
-		}
-
+		setting.setColor(10);
 		cout << "Type a word from language that you study: ";
+		setting.setColor(7);
 		cin >> learning_language_word;
 
+		auto seeking_word = dict.find(learning_language_word);
+		if (seeking_word != end(dict))
+		{
+			setting.setColor(4);
+			cout << "Word have already exists!" << endl;
+			setting.setColor(7);
+			return;
+		}
+
+		setting.setColor(10);
 		cout << "Type a word from language that your native: ";
+		setting.setColor(7);
 		cin >> native_language_word;
 
 		dict.insert({ learning_language_word, native_language_word });
@@ -61,51 +78,77 @@ public:
 	void DeleteAWord(map<string, string>& dict)
 	{
 		if (dict.size() == 0) {
+			setting.setColor(4);
 			cout << "Sorry you don't have any words yet!" << endl;
+			setting.setColor(7);
 			return;
 		}
 
 		string learning_language_word;
 
+		setting.setColor(10);
 		cout << "Type a word from language you study that you want to delete: ";
+		setting.setColor(7);
 		cin >> learning_language_word;
 
-		dict.erase(learning_language_word);
+		if (dict.erase(learning_language_word))
+		{
+			setting.setColor(6);
+			cout << "Word deleted successfully!" << endl;
+			setting.setColor(7);
+		}
+
+		else {
+			setting.setColor(4);
+			cout << "Word not found!" << endl;
+			setting.setColor(7);
+			return;
+		}
 	}
 
 
 	void EditAWord(map<string, string>& dict)
 	{
 		if (dict.size() == 0) {
+			setting.setColor(4);
 			cout << "Sorry you don't have any words yet!" << endl;
+			setting.setColor(7);
 			return;
 		}
 
 		string learning_language_word;
 
+		setting.setColor(10);
 		cout << "Type a learning word that you want to edit: ";
+		setting.setColor(7);
 		cin >> learning_language_word;
 
 		auto seeking_word = dict.find(learning_language_word);
 		if (seeking_word != end(dict))
 		{
 			string native_language_word;
+			setting.setColor(10);
 			cout << "Type a new translation of the word: ";
+			setting.setColor(7);
 			cin >> native_language_word;
 
 			dict[learning_language_word] = native_language_word;
 		}
 		else
 		{
+			setting.setColor(4);
 			cout << "Sorry we couldn't found the word, maybe"
 				"it's not exist, please type 2 and write it in this case." << endl;
+			setting.setColor(7);
 		}
 	}
 
 	void showAllWords(const map<string, string>& dict)
 	{
 		if (dict.size() == 0) {
+			setting.setColor(4);
 			cout << "Sorry you don't have any words yet!" << endl;
+			setting.setColor(7);
 			return;
 		}
 
@@ -119,6 +162,8 @@ public:
 
 class WorkingWithFiles
 {
+	GraphicSettings setting;
+
 public:
 	void ExportData(const map<string, string>& dict)
 	{
@@ -127,7 +172,10 @@ public:
 			for (auto it = dict.begin(); it != dict.end(); ++it)
 				data_file << it->first << "-" << it->second << "\n";
 			data_file.close();
+
+			setting.setColor(6);
 			cout << "Data exported successfully!" << endl;
+			setting.setColor(7);
 		}
 		else
 			cout << "Error!" << endl;
@@ -143,11 +191,16 @@ public:
 				data_file << it->first << "\n" << it->second << "\n";
 			}
 			data_file.close();
+
+			setting.setColor(6);
 			cout << "Data saved successfully!" << endl;
+			setting.setColor(7);
 		}
 		else
 		{
+			setting.setColor(4);
 			cout << "Data is not saved!" << endl;
+			setting.setColor(7);
 			Sleep(2000);
 		}
 	}
@@ -169,10 +222,39 @@ public:
 
 		file.close();
 	}
+
+	void deleteData(map<string, string>& dict)
+	{
+		setting.setColor(4);
+		cout << "Are you sure, that you want to delete everything? (1 - Yes, 2 - No): ";
+		setting.setColor(7);
+		int option;
+		cin >> option;
+
+		if (option == 1)
+		{
+			string fileName = "dictionary.dict";
+			ofstream data_file(fileName, ios::trunc);
+
+			if (data_file.is_open()) {
+				data_file.close();
+				dict = {};
+				setting.setColor(4);
+				cout << "Data have been reseted successfully!" << endl;
+				setting.setColor(7);
+			}
+		}
+		else
+		{
+			setting.setColor(6);
+			cout << "Your choise is not delete!" << endl;
+			setting.setColor(7);
+		}
+	}
 };
 
 
-class Menu : public WorkingWithWords, public WorkingWithFiles
+class Menu : public GraphicSettings, public WorkingWithWords, public WorkingWithFiles
 {
 protected:
 	int option;
@@ -180,24 +262,33 @@ protected:
 public:
 	Menu(int _option, map<string, string>& _dict) : option{ _option }, dict{ _dict } {}
 	Menu(map<string, string>& _dict) : option{ 0 }, dict{ _dict } {}
-	~Menu() { cout << "Exit!" << endl; }
+	~Menu() 
+	{ 
+		setColor(4);
+		cout << "Exit!" << endl; 
+		setColor(7);
+	}
 
 	void menu_self()
 	{
 		readData(dict);
 		cout << "------------------" << endl;
-		cout << "|      Menu	 |" << endl;
+		cout << "|      Menu      |" << endl;
 		cout << "------------------" << endl;
+		setColor(175);
 		cout << "Choose your option:" << endl;
+		setColor(9);
 		cout << "1.Find word" << endl;;
 		cout << "2.Add new word" << endl;
 		cout << "3.Delete a word" << endl;
 		cout << "4.Edit a word" << endl;
 		cout << "5.Show all words" << endl;
 		cout << "6.Export Data" << endl;
-		cout << "7.Exit" << endl;
-
-		cout << "Write your option (1, 2, 3, 4, 5, 6, 7): ";
+		cout << "7.Delete All Data" << endl;
+		cout << "8.Exit" << endl;
+		setColor(7);
+		
+		cout << "Write your option (1, 2, 3, 4, 5, 6, 7, 8): ";
 		cin >> option;
 
 		if (option == 1)
@@ -251,13 +342,19 @@ public:
 			menu_self();
 		}
 
+		if (option == 7)
+		{
+			system("cls");
+			deleteData(dict);
+			cout << endl;
+			menu_self();
+		}
+
 		else {
 			return;
 		}
 	}
 };
-
-
 
 
 int main()
